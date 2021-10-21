560405c21d0d60c7d587bced3a951064ee9e0121
Julio Zittei <julio.zittei96@gmail.com>
2021-10-20 21:38:54 -0300

    Add authentication and send message features

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,59 +1,66 @@
 import styles from "./styles.module.scss"
 import logo from "../../assets/logo.svg"
+import { api } from "../../services/api"
+import { useEffect, useState } from "react"
+import io from "socket.io-client"
+
+type Message = {
+  id: string
+  text: string
+  user: {
+    name: string
+    avatar_url: string
+  }
+}
+
+const messagesQueue: Message[] = []
+
+const socket = io("http://localhost:4000")
+
+socket.on("new_message", (newMessage: Message) => {
+  messagesQueue.push(newMessage)
+})
 
 export function MessageList(): JSX.Element {
+  const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages((prevState) =>
+          [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        )
+        messagesQueue.shift()
+      }
+    }, 3000)
+  }, [])
+
+  useEffect(() => {
+    api.get<Message[]>("messages").then((response) => {
+      setMessages(response.data)
+    })
+  }, [])
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={logo} alt="DoWhile 2021" />
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
-            tenetur autem accusamus voluptates repudiandae animi, nobis ratione
-            cumque. Repellendus a facere et, harum iusto atque ipsum. Temporibus
-            placeat non et.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/juliozittei.png"
-                alt="Julio Zittei"
-              />
-            </div>
-            <span>Julio Zittei</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
-            tenetur autem accusamus voluptates repudiandae animi, nobis ratione
-            cumque. Repellendus a facere et, harum iusto atque ipsum. Temporibus
-            placeat non et.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/juliozittei.png"
-                alt="Julio Zittei"
-              />
-            </div>
-            <span>Julio Zittei</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
-            tenetur autem accusamus voluptates repudiandae animi, nobis ratione
-            cumque. Repellendus a facere et, harum iusto atque ipsum. Temporibus
-            placeat non et.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/diego3g.png" alt="Julio Zittei" />
-            </div>
-            <span>Julio Zittei</span>
-          </div>
-        </li>
+        {messages.map((message) => {
+          return (
+            <li className={styles.message} key={message.id}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img
+                    src={`${message.user.avatar_url}`}
+                    alt={`${message.user.name}`}
+                  />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
